84ca74feb64705fc763d8a5267dcac3d93196b10
obar1 <mamatucci@gmail.com>
2024-10-19 07:32:07 +0200

    unit test refact

--- TEST.md
+++ TEST.md
@@ -1,1 +1,5 @@
-pytest -vs
+sudo apt-get -y install --no-install-recommends clang python3-clang
+
+PYTHONPATH="." VIZ=1 CLANG=1 pytest-watch
+
+PYTHONPATH="." VIZ=1 CLANG=1 ./simple_test.py
--- toonygrad/test/test_tiny.py
+++ toonygrad/test/test_tiny.py
@@ -3,12 +3,11 @@ from toonygrad import Tensor
 
 def test_tensor_addition():
     out = Tensor([1., 2, 3]) + Tensor([4., 5, 6])
-    out.tolist == [5.0, 7.0, 9.0]
+    assert out.tolist == [5.0, 7, 9]
 
 def test_tensor_def_output():
     Tensor([]) is not None
     print(Tensor([]))
-
 
 def test_tensor_addition_tensor_none():
     with pytest.raises(ValueError):
